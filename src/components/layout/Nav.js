ca8c26aec331cb340387eed19955549c5209ddd7
luba <lubo4ka.boiko@gmail.com>
2022-06-02 13:24:45 +0300

    start work on nav

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='nav flex'>
+      <div className='logo flex-item'>LOGO</div>
+      <div className='flex-item nav-links'>
+        <a href='#' className='nav-link'>Posts</a>
+        <a href='#' className='nav-link'>About</a>
+        <a href='#' className='nav-link'>Login</a>      
+      </div>
+    </nav>
   )
 }
 
