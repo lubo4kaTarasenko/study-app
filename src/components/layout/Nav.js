0b7b631ab891ab9ba36371e478df748e1b6cc9af
luba <lubo4ka.boiko@gmail.com>
2022-06-03 16:58:03 +0300

    responsive nav

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BaseBtn from '../buttons/BaseBtn'
 import { CgMenuGridO } from 'react-icons/cg'
 import { AiOutlineClose } from 'react-icons/ai'
 
 const Nav = () => {
+  const [showLinks, setShowLinks] = useState(false)
   return (
     <div className='navbar'>
       <div className='container flex'>
@@ -11,16 +12,17 @@ const Nav = () => {
           <img src="/download.png"/>
         </div>
         <nav >
-          <ul>
+          <ul className={`nav-links ${ !showLinks && 'hidden'}`}>
             <li><a href='#' className='nav-link'>Posts</a></li>
             <li><a href='#' className='nav-link'>About</a></li>
-            <li ><BaseBtn type='outlined' text='Login'/></li>   
+            <li className='login-btn-cont'><BaseBtn type='outlined' text='Login'/></li>   
           </ul>
         </nav>
+        <div className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
+          { showLinks ? <AiOutlineClose/> : <CgMenuGridO/>}
+        </div>
       </div>
-      <div className='nav-toggle'>
-        <CgMenuGridO/>
-      </div>
+     
     </div>
   )
 }
