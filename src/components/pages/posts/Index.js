dc234463bfd6e313af869ca276518d73303791cf
luba <lubo4ka.boiko@gmail.com>
2022-06-08 18:32:32 +0300

    work with .env

--- src/components/pages/posts/Index.js
+++ src/components/pages/posts/Index.js
@@ -1,6 +1,28 @@
 import React from 'react'
+import axios from 'axios'
+
 
 const Index = () => {
+
+  console.log(process.env.FAKE_JSON_TOKEN)
+  let payload = {
+    token: process.env.FAKE_JSON_TOKEN,
+    data: {
+      name: "nameFirst",
+      email: "internetEmail",
+      phone: "phoneHome",
+      _repeat: 20
+    }
+};
+
+axios({
+  method: "post",
+  url: "https://app.fakejson.com/q",
+  data: payload
+}).then(function(resp) {
+    
+  });
+
   return (
     <div>
       
